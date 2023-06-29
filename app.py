3a6a9753485166616e653e16e0d060b7bd051c91
Symbiotic Love <matthew@symbiotic.love>
2023-06-28 20:03:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)  # Enable CORS for all routes in the app
 
 @app.route('/api/data')
 def get_data():
-    data = {'message': 'Flask in the back.',
+    data = {'message': 'Flask in the back',
             'link': {
                 'href': 'https://github.com/SymbioticLove',
                 'text': 'View our other projects on GitHub!'
