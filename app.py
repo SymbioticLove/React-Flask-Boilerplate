51fe1ece0ec2b8dfcfd661d154d764b6166def01
Symbiotic Love <matthew@symbiotic.love>
2023-06-29 03:17:05 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ def get_data():
             'link': {
                 'href': 'https://github.com/SymbioticLove',
                 'text': 'View our other projects on GitHub!'
-            }}
+            },
+            'box1': "This is a boilerplate template for creating a web app using a React front-end and Flask back-end. Everything is already linked and proxied if you're running ports 3000/5000. All you need to do is clear out the return and data statements and code!",
+            'box2': "The batch file included in the 'shortcuts' directory, Initialize, will activate the virtual enviroment, install the necessary dependencies, launch the Flask server, launch the React app, and then disable the virtual environment to prevent accidental modifications",
+            }
     return jsonify(data)
     
-
 if __name__ == '__main__':
     app.run(debug=True)
