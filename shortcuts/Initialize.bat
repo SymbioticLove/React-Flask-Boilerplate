14f584eeea6caa539cdda6fcff6d4ad986808581
Symbiotic Love <matthew@symbiotic.love>
2023-06-28 20:04:36 -0700

    Update Initialize.bat

--- shortcuts/Initialize.bat
+++ shortcuts/Initialize.bat
@@ -10,13 +10,19 @@ rem Install Flask and Flask-Cors dependencies
 pip install flask flask_cors
 
 rem Install Node modules
-npm install
+cmd /c npm install
 
 rem Run "python app.py"
 start "Flask Server" cmd /c python app.py
 
 rem Change directory to "c:\React-Flask-Boilerplate-main"
 cd /d "c:\React-Flask-Boilerplate-main"
+
+rem Execute "npm start"
+start "React Server" cmd /c npm start
+
+rem Deactivate the virtual environment (optional)
+deactivate
 
 rem Execute "npm start"
 start "React Server" cmd /c npm start
